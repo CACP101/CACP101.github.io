17aa3662a66a5f00786eafd0d6d7a7b1c07c6148
Meguy3 <edward.sager@aol.com>
2025-07-11 15:17:39 -0400

    just random stuff

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-
+function animate(input) {
+    document.getElementById(input).style.animation = "colorchange 20s 20s forward"
+}
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+});
